e82eaddc00cf4b46f3fc7a1065b3bfed708cd9de
Julien Galego <julien.galego@technologypartner.io>
2023-10-09 15:54:17 +0200

    fix ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -15,4 +15,4 @@ export function getInfoByZipCode(zipCode: string): IZipCodeInfo[];
  * @deprecated Use {@link getInfos} instead
  */
 export function getInfoByCity(city: string): IZipCodeInfo[];
-export function getInfos(filter?: Partial<IZipCodeInfo>): string[];
+export function getInfos(filter?: Partial<IZipCodeInfo>): IZipCodeInfo[];
